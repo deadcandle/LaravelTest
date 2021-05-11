c3d38890e2499653e223172128a24adfbf946a04
koningstein <38209467+koningstein@users.noreply.github.com>
2021-05-11 09:44:06 +0200

    Fix opdracht 7

--- tests/Feature/Opdrachten/Opdracht7Test.php
+++ tests/Feature/Opdrachten/Opdracht7Test.php
@@ -10,8 +10,8 @@ beforeEach(function (){
 test('show page is visable with project on the page', function()
 {
     $this->withoutExceptionHandling();
-    $this->get(route('projects.index'))
-        ->assertViewIs('admin.projects.index')
+    $this->get(route('projects.show',['project' => $this->project->id]))
+        ->assertViewIs('admin.projects.show')
         ->assertSee($this->project->id)
         ->assertSee($this->project->name)
         ->assertSee($this->project->description)
